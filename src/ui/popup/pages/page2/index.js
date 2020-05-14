9e9a4f9554706e158a1c7def6364da3a74f6aaae
tigerHee <tigerhee@tigerHeedeMacBook-Pro.local>
2020-05-14 14:25:25 +0800

    useDispatch

--- src/ui/popup/pages/page2/index.js
+++ src/ui/popup/pages/page2/index.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
+import { FormattedMessage } from "react-intl";
+import { useDispatch } from "react-redux";
+import { update_state } from "@redux/actions";
 import styles from "./style.less";
 
 export default (props) => {
-
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      update_state({
+        currentLang: "en_US",
+      }),
+    );
+    return () => {
+      dispatch(
+        update_state({
+          currentLang: "zh_CN",
+        }),
+      );
+    };
+  }, []);
+
   const toPage1 = () => {
     chrome.browserAction.setBadgeText({ text: "" });
     chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
     // chrome.browserAction.enable()
     history.push("/");
   };
+
   return (
     <div className={styles.page}>
       <div className={styles.header}>PAGE 222</div>
@@ -18,6 +38,8 @@ export default (props) => {
         <button className={styles.btn} onClick={toPage1}>
           回page1
         </button>
+        <br />
+        <FormattedMessage id="hello" />
       </div>
       <div className={styles.footer}>footer</div>
     </div>
